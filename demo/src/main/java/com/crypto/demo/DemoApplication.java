db1dfc086c66afadfb2b824f899674f3a8c6f34d
geonsu.lee-at-603229842386 <ubuntu@GeonSu-Lee>
2024-06-21 09:27:28 +0900

    SpringBootServletInitializer 추가 in main

--- demo/src/main/java/com/crypto/demo/DemoApplication.java
+++ demo/src/main/java/com/crypto/demo/DemoApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SPringApplicationBuilder application) {
+		return application.sources(DenoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- dockerfile
+++ dockerfile
@@ -1,13 +1,10 @@
-FROM gradle:jdk22-alpine
-
-# 패키지 업데이트
-RUN apk update
+FROM openjdk:22-jdk
 
 # 디렉터리 변경
 WORKDIR /app
 
 # Build 아티팩트 복사
-COPY source dest
+COPY ./demo/build/libs/demo-0.0.1-SNAPSHOT.jar /app
 
 # Run Application
 CMD ["java", "-jar", "demo-0.0.1-SNAPSHOT.jar"]
